ce7d683ab8d76568f7aa27dc47d0dbc298725044
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-07 02:21:27 +0100

    update

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $posts,
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return response()->json([
+            'success' => true,
+            'results' => $post,
+        ]);
+    }
+}
